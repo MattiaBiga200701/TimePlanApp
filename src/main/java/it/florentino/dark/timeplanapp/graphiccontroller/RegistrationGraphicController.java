2bfd683bbf4a44f70421486f60a09b74338237dc
MattiaBiga200701 <mattia.biga@students.uniroma2.eu>
2024-05-22 00:48:37 +0200

    start Registration implementation

--- src/main/java/it/florentino/dark/timeplanapp/graphiccontroller/RegistrationGraphicController.java
+++ src/main/java/it/florentino/dark/timeplanapp/graphiccontroller/RegistrationGraphicController.java
@@ -3,16 +3,29 @@ package it.florentino.dark.timeplanapp.graphiccontroller;
 import it.florentino.dark.timeplanapp.ScenePlayer;
 import it.florentino.dark.timeplanapp.exceptions.SetSceneException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class RegistrationGraphicController {
 
-    ScenePlayer player = ScenePlayer.getScenePlayerInstance(null);
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField confirmPassword;
+
+    private ScenePlayer player = ScenePlayer.getScenePlayerInstance(null);
     @FXML
     public void onHyperLinkClicked(){
         try {
-            player.showScene("GUI/LoginPage.fxml", "StyleSheets/LoginStyle.css");
+            this.player.showScene("GUI/LoginPage.fxml", "StyleSheets/LoginStyle.css");
         }catch(SetSceneException sE){
             System.exit(-1);
         }
     }
+    @FXML
+    public void onRegisterButtonClick(){}
 }
