1b6e9399ce4bd3e586b7899a31e6dc292774a961
MattiaBiga200701 <mattia.biga@students.uniroma2.eu>
2024-06-11 13:27:41 +0200

    update RegistrationUseCase

--- src/main/java/it/florentino/dark/timeplanapp/graphiccontroller/RegistrationGraphicController.java
+++ src/main/java/it/florentino/dark/timeplanapp/graphiccontroller/RegistrationGraphicController.java
@@ -8,6 +8,7 @@ import it.florentino.dark.timeplanapp.exceptions.ServiceException;
 import it.florentino.dark.timeplanapp.exceptions.SetSceneException;
 import it.florentino.dark.timeplanapp.utils.enumaration.Role;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -24,35 +25,62 @@ public class RegistrationGraphicController extends GraphicController {
     private TextField confirmPassword;
     @FXML
     private Label errorLabel;
+    @FXML
+    private CheckBox checkRole;
+
+    private UserBean newUser;
 
     @FXML
     public void onHyperLinkClicked(){
         try {
             this.getScenePlayer().showScene("GUI/LoginPage.fxml");
         }catch(SetSceneException sE){
-            System.exit(-1);
+            throw new RuntimeException(sE);
         }
     }
-    @FXML
-    public void onRegisterButtonClick() {
 
+    @FXML
+    public void onNextButtonClick(){
         String usernameString = this.username.getText();
         String emailString = this.email.getText();
         String passwordString = this.password.getText();
         String retypedPassword = this.confirmPassword.getText();
-        UserBean newUser;
+        int control = 0;
+        try {
+
+            if(!(passwordString.equals(retypedPassword))) throw new CredentialException("Passwords do not match");
+
+            if(checkRole.isSelected()) {
+                this.newUser = new UserBean(usernameString, emailString, passwordString, Role.MANAGER);
+            }else{
+                this.newUser = new UserBean(usernameString, emailString, passwordString, Role.EMPLOYEE);
+            }
+
+        } catch (CredentialException e) {
+            this.showError(e.getMessage());
+            control = 1;
+        }
+
+        if(control == 0) {
+            try {
+                this.getScenePlayer().showScene("GUI/RegistrationPage2.fxml");
+            } catch (SetSceneException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+    @FXML
+    public void onRegisterButtonClick() {
+
         RegistrationController controller = new RegistrationController();
         try {
 
-            if( passwordString.equals(retypedPassword)) {
-                newUser = new UserBean(usernameString, emailString, passwordString, Role.EMPLOYEE);
-            }else{ throw new CredentialException("Passwords do not match"); }
-            controller.insertUser(newUser);
+            controller.insertUser(this.newUser);
             System.out.println("User written");
 
-
-        } catch (CredentialException | ServiceException e) {
-            this.showError(e.getMessage());
+        } catch (ServiceException e) {
+            throw new RuntimeException(e);
         }
     }
     public Label getErrorLabel(){
