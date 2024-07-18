069343b8f8028c871a64bed011138af49e8d7845
MattiaBiga200701 <mattia.biga@students.uniroma2.eu>
2024-07-18 12:06:11 +0200

    Test-Case implementation

--- src/main/java/it/florentino/dark/timeplanapp/exceptions/NotUniqueEmailException.java
+++
@@ -1,6 +0,0 @@
-package it.florentino.dark.timeplanapp.exceptions;
-
-public class NotUniqueEmailException extends Exception{
-
-    public NotUniqueEmailException() { super("Email already exist"); }
-}
